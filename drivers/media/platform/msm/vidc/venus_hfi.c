29a13bff6ac2f16b8615f4d9f8abc872433b6665
Sultan Alsawaf <sultan@kerneltoast.com>
2018-11-23 03:32:12 -0800

    msm: vidc: Fix bus_vote memory leaks
    The memory allocated to bus_vote.data can be leaked in multiple different
    ways, and trying to patch all of those possible leaks is too difficult
    to manage.
    
    Since bus_vote.data can only contain up to MAX_SUPPORTED_INSTANCES elements,
    just use a fixed array instead of trying to dynamically allocate memory.
    
    This fixes the following memory leak:
    unreferenced object 0xffffffd403d0f780 (size 128):
      comm "vendor.qti.medi", pid 827, jiffies 4294879400 (age 81.376s)
      hex dump (first 32 bytes):
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
      backtrace:
        [<ffffff81d67fbc80>] kmem_cache_alloc_trace+0x218/0x2a0
        [<ffffff81d6feb9fc>] venus_hfi_core_init+0x64/0x1538
        [<ffffff81d6fcd264>] msm_comm_try_state+0x63c/0x1178
        [<ffffff81d6fd73d8>] msm_vidc_open+0x5a8/0x6f8
        [<ffffff81d6fc3208>] msm_v4l2_open+0x38/0x200
        [<ffffff81d6f8b2d0>] v4l2_open+0xa0/0x110
        [<ffffff81d6818bec>] chrdev_open+0x7c/0x178
        [<ffffff81d6810714>] do_dentry_open+0x104/0x2e8
        [<ffffff81d6811e2c>] vfs_open+0x44/0x70
        [<ffffff81d6821b34>] path_openat+0x2ec/0x1088
        [<ffffff81d6823648>] do_filp_open+0x78/0xf0
        [<ffffff81d68123b8>] do_sys_open+0x170/0x268
        [<ffffff81d6873124>] compat_SyS_openat+0xc/0x18
        [<ffffff81d6682f40>] el0_svc_naked+0x34/0x38
        [<ffffffffffffffff>] 0xffffffffffffffff
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/media/platform/msm/vidc/venus_hfi.c
+++ drivers/media/platform/msm/vidc/venus_hfi.c
@@ -74,11 +74,6 @@ struct tzbsp_video_set_state_req {
 	u32 spare; /* reserved for future, should be zero */
 };
 
-const struct msm_vidc_gov_data DEFAULT_BUS_VOTE = {
-	.data = NULL,
-	.data_count = 0,
-};
-
 const int max_packets = 1000;
 
 static void venus_hfi_pm_handler(struct work_struct *work);
@@ -828,8 +823,6 @@ static int __unvote_buses(struct venus_hfi_device *device)
 	int rc = 0;
 	struct bus_info *bus = NULL;
 
-	kfree(device->bus_vote.data);
-	device->bus_vote.data = NULL;
 	device->bus_vote.data_count = 0;
 
 	venus_hfi_for_each_bus(device, bus) {
@@ -853,7 +846,6 @@ static int __vote_buses(struct venus_hfi_device *device,
 {
 	int rc = 0;
 	struct bus_info *bus = NULL;
-	struct vidc_bus_vote_data *new_data = NULL;
 
 	if (!num_data) {
 		dprintk(VIDC_DBG, "No vote data available\n");
@@ -863,16 +855,7 @@ static int __vote_buses(struct venus_hfi_device *device,
 		return -EINVAL;
 	}
 
-	new_data = kmemdup(data, num_data * sizeof(*new_data), GFP_KERNEL);
-	if (!new_data) {
-		dprintk(VIDC_ERR, "Can't alloc memory to cache bus votes\n");
-		rc = -ENOMEM;
-		goto err_no_mem;
-	}
-
 no_data_count:
-	kfree(device->bus_vote.data);
-	device->bus_vote.data = new_data;
 	device->bus_vote.data_count = num_data;
 
 	venus_hfi_for_each_bus(device, bus) {
@@ -1706,16 +1689,8 @@ static int venus_hfi_core_init(void *device)
 
 	mutex_lock(&dev->lock);
 
-	dev->bus_vote.data =
-		kzalloc(sizeof(struct vidc_bus_vote_data), GFP_KERNEL);
-	if (!dev->bus_vote.data) {
-		dprintk(VIDC_ERR, "Bus vote data memory is not allocated\n");
-		rc = -ENOMEM;
-		goto err_no_mem;
-	}
-
 	dev->bus_vote.data_count = 1;
-	dev->bus_vote.data->power_mode = VIDC_POWER_TURBO;
+	dev->bus_vote.data[0].power_mode = VIDC_POWER_TURBO;
 
 	rc = __load_fw(dev);
 	if (rc) {
@@ -1790,7 +1765,6 @@ err_core_init:
 	__set_state(dev, VENUS_STATE_DEINIT);
 	__unload_fw(dev);
 err_load_fw:
-err_no_mem:
 	dprintk(VIDC_ERR, "Core init failed\n");
 	mutex_unlock(&dev->lock);
 	return rc;
@@ -3451,8 +3425,7 @@ static void __deinit_bus(struct venus_hfi_device *device)
 	if (!device)
 		return;
 
-	kfree(device->bus_vote.data);
-	device->bus_vote = DEFAULT_BUS_VOTE;
+	device->bus_vote.data_count = 0;
 
 	venus_hfi_for_each_bus_reverse(device, bus) {
 		devfreq_remove_device(bus->devfreq);
@@ -3529,7 +3502,7 @@ static int __init_bus(struct venus_hfi_device *device)
 		devfreq_suspend_device(bus->devfreq);
 	}
 
-	device->bus_vote = DEFAULT_BUS_VOTE;
+	device->bus_vote.data_count = 0;
 	return 0;
 
 err_add_dev:
